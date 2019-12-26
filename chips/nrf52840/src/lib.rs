df6d6d1d545edf8e659a55dab73bbd9bbe8d713e
Langston Nashold <lgnashold@gmail.com>
2019-12-26 03:23:04 -0600

    Add analog comparator driver for nrf52 chips

--- chips/nrf52840/src/lib.rs
+++ chips/nrf52840/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 pub use nrf52::{
-    adc, aes, ble_radio, clock, constants, crt1, ficr, i2c, ieee802154_radio, init, nvmc, pinmux,
+    acomp, adc, aes, ble_radio, clock, constants, crt1, ficr, i2c, ieee802154_radio, init, nvmc, pinmux,
     ppi, pwm, rtc, spi, temperature, timer, trng, uart, uicr,
 };
 pub mod chip;
