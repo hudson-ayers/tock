bb074c5f1f137ed702c606b8404257611b48674d
Hudson Ayers <hayers@stanford.edu>
2020-05-01 08:45:25 -0700

    remove legacy code

--- tools/github_actions_size_changes.sh
+++ tools/github_actions_size_changes.sh
@@ -5,8 +5,6 @@
 # the target branch before and after merging in the PR.
 # This script only reports updates for boards whose size have changed as a result of the PR being
 # tested, and does not currently support analyzing size differences in RISC-V boards.
-# This file relies on a travis enviroment variable to post to github, which is the value of a
-# Github OAuth personal token associated with @hudson-ayers Github identity.
 
 set -e
 
@@ -42,10 +40,6 @@ for elf in $(find . -maxdepth 8 | grep 'release' | egrep '\.elf$' | grep -v 'ris
     ./tools/diff_memory_usage.py previous-benchmark-${b} current-benchmark-${b} size-diffs-${b}.txt ${b}
     if [ -s "size-diffs-${b}.txt" ]; then
         RES="$( grep -hs ^ size-diffs-${b}.txt )" #grep instead of cat to prevent errors on no match
-        #if [ -n "${TRAVIS_GITHUB_TOKEN}" ]; then
-            # Only attempt to post statuses if the token is available (will not post for PRs from forks)
-        #    curl -X POST -H "Content-Type: application/json" --header "Authorization: token ${TRAVIS_GITHUB_TOKEN}" --data '{"state": "success", "context": "'"${b}"'", "description": "'"${RES}"'"}' https://api.github.com/repos/tock/tock/statuses/${TRAVIS_PULL_REQUEST_SHA}
-        #fi
         echo "${b}: ${RES}"
     fi
 done
